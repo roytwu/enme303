e0dd274c8bb47bbc4165f07cfb433657538e1b8a
Roy Wu <roywu2018@gmail.com>
2025-04-08 23:55:02 -0400

    completely overhaul the demo exercises

--- demo_independence.m
+++ demo_independence.m
@@ -2,34 +2,41 @@
 %* Author:      Roy Wu
 %* Description: Demo of linear independence 
 %* History:     04/15/2024 initial version 
+%*              04/08/2025 Completely overhaul the exercises
 %* ========== ========== ==========
 clc; clear; close;
 format short
 
-%* column vectors
-u = [3;   1;  0; 5;  2;   -8];
-v = [-1;  2;  8; 60; 2;   1];
-x = [1;   4;  4; 1;  345; 0];
-y = [345; 2; -4; 0;  7;   99];
-z = 2*u-2*v-x;
-display(z)
+%% column vectors
+u = [3;  1;  0; 5;  2];
+v = [-1; 2;  8; 3;  0];
+q = [0;  4;  4; 1;  2];
+r = [1;  1;  1; 0;  0];
+s = [0; 2;  -2; 4; 0];
+A = [u v q r s];
+x = [1; 0; 2; -2; 0];
 
-%* Use row reduction to find linear independence
-A1 = [u'; v'; x'];
-A2 = [u'; v'; x'; y'; z'];
+b = A*x;
+display(b)
+
+
+%% Use row reduction to find linear independence
+A1 = [q'; r'; s'];      %* 3 vectors
+A2 = [u'; q'; r'; b'];  %* 4 vectors 
+A3 = [u'; v'; A1];      %* 5 vectors
 
 %* Number of linearly independent rows is equal to 
 %* number of linearly independent columns
-A3 = transpose([A1; y'; z']);
-
-disp('reduced A is...')
 disp(rref(A1))
+disp(rref(A1'))
+
+
 disp(rref(A2))
+disp(rref(A2'))
+
+
 disp(rref(A3))
-
-
-
-
+disp(rref(A3'))
 
 
 
