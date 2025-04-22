84b140852a56a063833442ae325fd4757f0c26e3
Roy Wu <roywu2018@gmail.com>
2025-04-22 19:11:52 -0400

    adding demo script for eigenvalues and eigenvectors

---
+++ demo_eigen.m
@@ -0,0 +1,38 @@
+%* ========== ========== ==========
+%* Author: 　　　Roy Wu
+%* Description: rpeated eigen value demo
+%* History:     11/16/2021 initial version 
+%*              05/06/2024 Adding one more example 
+%* ========== ========== ==========
+
+%% Lecture Demo
+%* assemble the matrix A
+a1 = [1; 0; 1; 1];
+a2 = [0; 1; 0; 0];
+a3 = [0; 5; 2; 0];
+a4 = [0; -10; 0; 3];
+A = [a1 a2 a3 a4];
+
+%* lambda=1, value is founded by hand, repeated twice
+eV_a = [-2; 0; 2; 1]; %* corresponding eigenvector a
+eV_b = [0; 1; 0; 0];  %* corresponding engevector b
+
+s = 3;               %* 1st free variable 
+t = 27;              %* 2nd free variable
+v = s*eV_a+t*eV_b;   %* linear cobo
+disp('eigenvector is...');
+disp(v)
+
+disp('eigenvalue equation shows...');
+disp(A*v)
+
+%% the eig() function
+[out1, out2] = eig(A);
+
+%* display numbers in string for better alignment
+%* for cosmetic purpose only
+out1 = num2str(out1,'%10.3f'); 
+
+disp(out1)
+disp(out2)
+% disp(out1(:,1))
