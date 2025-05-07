e87f85dbfb9870a2ce7c715579ec07a2f4cfc3b7
roytwu <roywu2018@gmail.com>
2025-05-07 08:20:36 -0400

    updating signifincant digit number

--- gauss_seidel/demo_GS.m
+++ gauss_seidel/demo_GS.m
@@ -13,23 +13,23 @@ sDigit = 2;
 out    = round(a, sDigit, 'significant');
 disp(out)
 
-%% Example #1, 3-by-3 Well-Posed Matx
+%% Example #1, 3-by-3 Well-Posed Matrix
 clc; clear;
 A = [5 -2 3; -3 9 1; 2 -1 -7];
 b = [-1; 2; 3];
 augA = [A b];
-x=A\b;
+x = A\b;
 display(x)
 
 %* Gauss-Seidel method (user-defined function)
-sDigit = 4;                       %* singnificant digit
-itr    = 10;                      %* number of iteration
+sDigit = 3;        %* singnificant digit (using 3 for the slides)
+itr    = 10;       %* number of iteration
 out    = gSeidel(itr, augA, sDigit); %* augA must be 3 by 4
 fprintf('Iteration output is...\n');
 disp(out);
 % disp(out(:,end));
 
-%% Example #2,3-by-3 Ill-Posed Matx
+%% Example #2, 3-by-3 Ill-Posed Matrix
 clc; clear;
 A = [1 1 0; 1 41/40 0; 0 1 31/30];
 x = [-80; 90; 60];
@@ -38,8 +38,8 @@ augA = [A b];
 
 
 %* Gauss-Seidel method (user-defined function)
-sDigit = 6;                       %* singnificant digit
-itr    = 450;                      %* number of iteration
+sDigit = 5;        %* singnificant digit
+itr    = 350;      %* number of iteration
 out    = gSeidel(itr, augA, sDigit); %* augA must be 3 by 4
 fprintf('Iteration output is...\n');
 disp(out);
