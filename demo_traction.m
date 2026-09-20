4fd2949b24d667569a695a4411842ba84115c618
roywu <roywu2018@gmail.com>
2026-09-20 14:44:38 -0400

    adding example of max shear

--- demo_traction.m
+++ demo_traction.m
@@ -1,5 +1,5 @@
 %format rat
-format short
+%format short
 %% Problem set up
 Sig = [80 30; 30 40];
 n = [1/2; sqrt(3)/2];
@@ -52,3 +52,33 @@ display(pAxis)
 %% Principal Stress (matrix form)
 clc;
 [vec, val]=eig(Sig)
+
+
+%% HW Problem - Biaxial Stresses
+clc; clear;
+format bank
+Sig = [3600 0; 0 -1600];
+theta = 56.31;
+
+n =[cosd(theta); sind(theta)];
+
+
+%* 
+t = Sig*n
+display(norm(t))
+
+%* alternatively
+R =[cosd(90) -sind(90); sind(90) cosd(90)];
+sig_x = n.'* Sig*n;
+tau_xy = n.'* Sig*R*n;
+
+
+% %* max shear
+% [vec, val] = eig(Sig);
+% 
+% n =[cosd(45); sind(45)]; %* direction of max shear
+% R =[cosd(90) -sind(90); sind(90) cosd(90)];
+% 
+% 
+% sig_x = n.'* Sig*n
+% tau_xy = n.'* Sig*R*n
