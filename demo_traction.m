ed91a8c293b89bebef37725e552428441eccac5c
roywu <roywu2018@gmail.com>
2026-09-22 13:30:35 -0400

    adding comments, tidy up

--- demo_traction.m
+++ demo_traction.m
@@ -1,24 +1,27 @@
 %format rat
 %format short
 %% Problem set up
+
 Sig = [80 30; 30 40];
 n = [1/2; sqrt(3)/2];
 
-%* traction
+%* find traction
 t = Sig*n;
 
-%* magnitude of normal stress
+%* find magnitude of normal stress
 sig_x1 = n.'*Sig*n;
 
 
 %% shear stress
+clc;
+%* Find shear stress from traction vector
 tau_x1y1 = sqrt(norm(t)^2- sig_x1^2); 
 display(tau_x1y1)
 
 byHand = -10*sqrt(3)-15;
 display(byHand)
 
-%* alternatively, rotate n 90 degress to get the direction of shear stress
+%* Alternatively, rotate n 90 degress to get the direction of shear stress
 %* Then project t onto the direction of the shear stress
 angle= pi/2;
 R  = [cos(angle) -sin(angle); sin(angle) cos(angle)];
@@ -35,6 +38,7 @@ sig_x = Sig(1,1);
 sig_y = Sig(2,2);
 tau_xy = Sig(1,2);
 
+%* equation from ENME220
 dumm = (sig_x-sig_y)/2;
 sig1 = (sig_x+sig_y)/2 +  sqrt((dumm)^2+tau_xy^2)
 sig2 = (sig_x+sig_y)/2 -  sqrt((dumm)^2+tau_xy^2)
@@ -42,9 +46,10 @@ sig2 = (sig_x+sig_y)/2 -  sqrt((dumm)^2+tau_xy^2)
 
 %* principal angle
 dumm = 2*tau_xy/(sig_x-sig_y);
-%theta_p = atan(dumm)/2;
+theta_p = atan(dumm)/2;
 display(theta_p)
 
+%* principal axis
 unit_x = cos(theta_p);
 unit_y = sin(theta_p);
 pAxis = [unit_x; unit_y]
@@ -82,3 +87,10 @@ tau_xy = n.'* Sig*R*n;
 % 
 % sig_x = n.'* Sig*n
 % tau_xy = n.'* Sig*R*n
+
+%% Pure Torsion
+clc;
+syms t
+sig = [0 t 0; t 0 0; 0 0 0];
+
+[vec, val]=eig(sig)
